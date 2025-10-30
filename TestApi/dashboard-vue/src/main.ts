178c5979d5577fe9e7649325ee741522e300711a
khongvan-dt <khongvandt14082004@gmail.com>
2025-10-30 15:15:59 +0700

    add code

--- TestApi/dashboard-vue/src/main.ts
+++ TestApi/dashboard-vue/src/main.ts
@@ -1,31 +1,29 @@
 import './assets/css/main.css'
-
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import ui from '@nuxt/ui/vue-plugin'
 
 import App from './App.vue'
 
-const app = createApp(App)
- 
-app.use(createRouter({
+// === Router ===
+const router = createRouter({
+  history: createWebHistory(),
   routes: [
-     { path: '/', component: () => import('./pages/index.vue') },
-          { path: '/dashboard', component: () => import('./pages/Dashboard.vue') },
-
-      {
+    { path: '/', component: () => import('./pages/index.vue') },
+     {
       path: '/settingjob',
       component: () => import('./pages/settingJob.vue'),
       children: [
         { path: '', component: () => import('./pages/settingsjob/index.vue') },
-         { path: 'notifications', component: () => import('./pages/settingsjob/notifications.vue') },
+        { path: 'notifications', component: () => import('./pages/settingsjob/notifications.vue') },
         { path: 'security', component: () => import('./pages/settingsjob/security.vue') },
       ]
     }
-  ],
-  history: createWebHistory()
-}))
+  ]
+})
 
+// === App ===
+const app = createApp(App)
+app.use(router)   
 app.use(ui)
-
 app.mount('#app')
